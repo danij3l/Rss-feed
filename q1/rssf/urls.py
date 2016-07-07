06c18cfd15d4915b5cc06e2e00f48382d43628ee
danij3l <danijelpavic21@gmail.com>
2016-07-07 17:56:44 +0200

    added search_by_author view

--- q1/rssf/urls.py
+++ q1/rssf/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import rssf_list, FeedsListView, FeedDetailView
+from .views import rssf_list, FeedsListView, FeedDetailView, search
 
 urlpatterns = [
 	url(r'^$', rssf_list, name='rssf_list'),
     url(r'feeds/$', FeedsListView.as_view(), name='feeds'),
     url(r'feed/(?P<pk>\d+)/$', FeedDetailView.as_view(), name='feed'),
+    url(r'author/$', search, name='search'),
 ]
